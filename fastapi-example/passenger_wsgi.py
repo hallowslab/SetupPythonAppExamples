ee1ffadb6b2118a13d0b8138b9dbc3dcc95eea54
hallowslab <hallows.tech.lab@gmail.com>
2025-05-25 17:39:57 +0100

    Fix ASGI conversion

--- fastapi-example/passenger_wsgi.py
+++ fastapi-example/passenger_wsgi.py
@@ -1,5 +1,4 @@
-from starlette.middleware.wsgi import WSGIMiddleware
+from a2wsgi import WSGIMiddleware
 from main import app as fastapi_app
 
-# Convert FastAPI (ASGI) app to WSGI-compatible app
 application = WSGIMiddleware(fastapi_app)
