292040d8a1c1b845a778da8e79feffcf45f998c7
hallowslab <hallows.tech.lab@gmail.com>
2025-06-01 15:52:23 +0100

    add_book endpoint

--- django-example/library/views.py
+++ django-example/library/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Book
 
-@login_required
 def book_list(request):
     books = Book.objects.all()
     return render(request, "library/book_list.html", {"books": books})
+
+@login_required
+def add_book(request):
+    if request.method == "POST":
+        title = request.POST.get("title", "").strip()
+        author = request.POST.get("author", "").strip()
+        if title and author:
+            Book.objects.create(title=title, author=author)
+            return redirect("book_list")
+    return render(request, "library/add_book.html")
