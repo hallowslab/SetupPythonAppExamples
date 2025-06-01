aa73e5c271d67f7a1221a7274da011d38bf96d06
hallowslab <hallows.tech.lab@gmail.com>
2025-06-01 15:34:23 +0100

    update fastapi, replace logging with print to be different than flask-example, add handled and unhandled endpoints to test exceptions

--- fastapi-example/main.py
+++ fastapi-example/main.py
@@ -1,31 +1,41 @@
-import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-# Configure basic logging
-logging.basicConfig(
-    level=logging.INFO,  # Change to DEBUG for more detail
-    format="%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d %(message)s",
-    handlers=[
-        logging.FileHandler("app.log"),  # Logs to the specified file
-        logging.StreamHandler()          # If stream is not specified, sys.stderr is used (useful in cPanel logs)
-    ]
-)
-
-logger = logging.getLogger(__name__)
-
 @app.get("/")
 def home():
-    logger.info(f"Request for /, displaying version {app.version}")
+    print(f"Request for /, displaying version {app.version}")
     return {"message": f"FastAPI, version: {app.version}"}
 
 @app.get("/ping")
 def ping():
-    logger.info(f"Request for /ping, replying with pong!")
+    print(f"Request for /ping, replying with pong!")
     return {"message": "pong!"}
 
 @app.get("/hello")
 def hello(name: str = "world"):
-    logger.info(f"request at /hello from {name}")
+    print(f"request at /hello from {name}")
     return {"message": f"Hello, {name}!"}
+
+@app.post("/add")
+async def calc(request: Request):
+    data = await request.json()
+    print(f"request at /add for {data}")
+    try:
+        x = data.get("x", None)
+        y = data.get("y", None)
+        if x is None or y is None:
+            raise ValueError(f"Missing values: {x}:{y}")
+        res = x+y
+        return {"message": res}
+    except Exception as e:
+        print(f"/add failed because: {e}")
+        return {"message": f"Error: {e}"}
+    
+@app.post("/unadd")
+async def bcalc(request: Request):
+    data = await request.json()
+    x = data.get("x", None)
+    y = data.get("y", None)
+    print(f"request at /add for {x}+{y}")
+    return {"message": x+y}
